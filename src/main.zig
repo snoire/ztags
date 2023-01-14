c05210ac9546f8792462077db8e1d597fe8792f8
snoire <snoire@qq.com>
2023-01-14 23:24:40 +0800

    recognize structs returned from functions

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ const ScopeList = std.ArrayList(struct {
     kind: []const u8,
     scope: []const u8,
 });
+const Error = std.os.WriteError || error{OutOfMemory};
 
 var ast: Ast = undefined;
 var stack: ScopeList = undefined;
@@ -59,95 +60,33 @@ fn printTags(index: Node.Index) !void {
         },
 
         .simple_var_decl => {
-            const init_node_tag = ast.nodes.items(.tag)[data.rhs];
-            const init_node_token = ast.nodes.items(.main_token)[data.rhs];
-            const init_node_data = ast.nodes.items(.data)[data.rhs];
-
+            const init_node = data.rhs;
             const public = if (ast.fullVarDecl(index).?.visib_token) |_| true else false;
 
-            switch (init_node_tag) {
-                .container_decl_two,
-                .container_decl_two_trailing,
-                .container_decl,
-                .container_decl_trailing,
-                .container_decl_arg,
-                .container_decl_arg_trailing,
-                .tagged_union_two,
-                .tagged_union_two_trailing,
-                .tagged_union,
-                .tagged_union_trailing,
-                .tagged_union_enum_tag,
-                .tagged_union_enum_tag_trailing,
-                => {
-                    // const a = `struct {}`, `union {}`, `enum {}` or `opaque {}`
-                    try printLine(.{
-                        .tag = main_token + 1,
-                        .kind = ast.tokenSlice(init_node_token),
-                        .public = public,
-                    });
+            if (isContainer(init_node)) {
+                try printContainer(main_token + 1, init_node, public);
+            } else {
+                const init_node_tag = ast.nodes.items(.tag)[init_node];
+                switch (init_node_tag) {
+                    .error_set_decl => {
+                        try printLine(.{
+                            .tag = main_token + 1,
+                            .kind = "error",
+                            .public = public,
+                        });
+                    },
 
-                    try stack.append(.{
-                        .kind = ast.tokenSlice(init_node_token),
-                        .scope = ast.tokenSlice(main_token + 1),
-                    });
-                    defer _ = stack.pop();
-
-                    // print tags of their ContainerMembers
-                    switch (init_node_tag) {
-                        .container_decl_two,
-                        .container_decl_two_trailing,
-                        .tagged_union_two,
-                        .tagged_union_two_trailing,
-                        => {
-                            if (init_node_data.lhs > 0) try printTags(init_node_data.lhs);
-                            if (init_node_data.rhs > 0) try printTags(init_node_data.rhs);
-                        },
-                        .container_decl,
-                        .container_decl_trailing,
-                        .tagged_union,
-                        .tagged_union_trailing,
-                        => {
-                            for (ast.extra_data[init_node_data.lhs..init_node_data.rhs]) |member| {
-                                try printTags(member);
-                            }
-                        },
-                        .container_decl_arg,
-                        .container_decl_arg_trailing,
-                        .tagged_union_enum_tag,
-                        .tagged_union_enum_tag_trailing,
-                        => {
-                            const params = ast.extraData(init_node_data.rhs, Node.SubRange);
-                            for (ast.extra_data[params.start..params.end]) |member| {
-                                try printTags(member);
-                            }
-                        },
-                        else => unreachable,
-                    }
-                },
-
-                .error_set_decl => {
-                    try printLine(.{
-                        .tag = main_token + 1,
-                        .kind = "error",
-                        .public = public,
-                    });
-                },
-
-                // `var` or `const`
-                else => {
-                    try printLine(.{
-                        .tag = main_token + 1,
-                        .kind = ast.tokenSlice(main_token),
-                        .public = public,
-                    });
-                },
+                    // `var` or `const`
+                    else => {
+                        try printLine(.{
+                            .tag = main_token + 1,
+                            .kind = ast.tokenSlice(main_token),
+                            .public = public,
+                        });
+                    },
+                }
             }
         },
-
-        .container_field_init => try printLine(.{
-            .tag = main_token,
-            .kind = "field",
-        }),
 
         .fn_proto_simple,
         .fn_proto_multi,
@@ -157,13 +96,60 @@ fn printTags(index: Node.Index) !void {
         => {
             var buf: [1]Node.Index = undefined;
             const full = ast.fullFnProto(&buf, index).?;
+            const public = if (full.visib_token) |_| true else false;
 
-            try printLine(.{
-                .tag = main_token + 1,
-                .kind = "function",
-                .public = if (full.visib_token) |_| true else false,
-            });
+            // a function may return a struct
+            const container_node: ?Node.Index = blk: {
+                const return_type_main_token = ast.nodes.items(.main_token)[full.ast.return_type];
+
+                if (std.mem.eql(u8, ast.tokenSlice(return_type_main_token), "type")) {
+                    const block_node = data.rhs;
+                    const block_node_tag = ast.nodes.items(.tag)[block_node];
+                    const block_node_data = ast.nodes.items(.data)[block_node];
+
+                    const statements: []const Node.Index = switch (block_node_tag) {
+                        .block_two,
+                        .block_two_semicolon,
+                        => &.{ block_node_data.lhs, block_node_data.rhs },
+
+                        .block,
+                        .block_semicolon,
+                        => ast.extra_data[block_node_data.lhs..block_node_data.rhs],
+
+                        else => unreachable,
+                    };
+
+                    for (statements) |statement| {
+                        if (statement == 0) continue;
+
+                        const statement_tag = ast.nodes.items(.tag)[statement];
+                        if (statement_tag == .@"return") {
+                            const statement_data = ast.nodes.items(.data)[statement];
+
+                            if (isContainer(statement_data.lhs)) {
+                                break :blk statement_data.lhs;
+                            }
+                        }
+                    }
+                }
+                break :blk null;
+            };
+
+            if (container_node) |container| {
+                try printContainer(main_token + 1, container, public);
+            } else {
+                try printLine(.{
+                    .tag = main_token + 1,
+                    .kind = "function",
+                    .public = public,
+                });
+            }
         },
+
+        .container_field_init => try printLine(.{
+            .tag = main_token,
+            .kind = "field",
+        }),
 
         .test_decl => try printLine(.{
             .tag = if (data.lhs > 0) data.lhs else main_token,
@@ -177,11 +163,82 @@ fn printTags(index: Node.Index) !void {
     }
 }
 
+fn isContainer(node: Node.Index) bool {
+    const tag = ast.nodes.items(.tag)[node];
+    return switch (tag) {
+        .container_decl_two,
+        .container_decl_two_trailing,
+        .container_decl,
+        .container_decl_trailing,
+        .container_decl_arg,
+        .container_decl_arg_trailing,
+        .tagged_union_two,
+        .tagged_union_two_trailing,
+        .tagged_union,
+        .tagged_union_trailing,
+        .tagged_union_enum_tag,
+        .tagged_union_enum_tag_trailing,
+        => true,
+
+        else => false,
+    };
+}
+
+fn printContainer(tag: Ast.TokenIndex, container: Node.Index, public: bool) Error!void {
+    const container_tag = ast.nodes.items(.tag)[container];
+    const container_token = ast.nodes.items(.main_token)[container];
+    const container_data = ast.nodes.items(.data)[container];
+
+    // const A = `struct {}`, `union {}`, `enum {}` or `opaque {}`
+    try printLine(.{
+        .tag = tag,
+        .kind = ast.tokenSlice(container_token),
+        .public = public,
+    });
+
+    try stack.append(.{
+        .kind = ast.tokenSlice(container_token),
+        .scope = ast.tokenSlice(tag),
+    });
+    defer _ = stack.pop();
+
+    // print tags of their ContainerMembers
+    const container_members: []const Node.Index = switch (container_tag) {
+        .container_decl_two,
+        .container_decl_two_trailing,
+        .tagged_union_two,
+        .tagged_union_two_trailing,
+        => &.{ container_data.lhs, container_data.rhs },
+
+        .container_decl,
+        .container_decl_trailing,
+        .tagged_union,
+        .tagged_union_trailing,
+        => ast.extra_data[container_data.lhs..container_data.rhs],
+
+        .container_decl_arg,
+        .container_decl_arg_trailing,
+        .tagged_union_enum_tag,
+        .tagged_union_enum_tag_trailing,
+        => blk: {
+            const params = ast.extraData(container_data.rhs, Node.SubRange);
+            break :blk ast.extra_data[params.start..params.end];
+        },
+
+        else => unreachable,
+    };
+
+    for (container_members) |member| {
+        if (member == 0) continue;
+        try printTags(member);
+    }
+}
+
 fn printLine(info: struct {
     tag: Ast.TokenIndex,
     kind: []const u8,
     public: bool = false,
-}) !void {
+}) std.os.WriteError!void {
     const loc = ast.tokenLocation(0, info.tag);
     try writer.print(
         "{s}\t{s}\t{};\"\t{s}\tline:{}\tcolumn:{}",
